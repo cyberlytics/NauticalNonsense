7b30394e4b712fc1b5265c2266c4b6f7fe506411
Maximilian Schlosser <m.schlosser@oth-aw.de>
2023-05-15 15:54:08 +0200

    add module to use ec2 as nat instance

--- sys_src/infrastructure/eks.tf
+++ sys_src/infrastructure/eks.tf
@@ -115,6 +115,23 @@ module "eks" {
   }
 }
 
+module "nat" {
+  source = "./nat-instance"
+
+  name                        = "main"
+  vpc_id                      = module.vpc.vpc_id
+  public_subnet               = module.vpc.public_subnets[0]
+  private_subnets_cidr_blocks = module.vpc.private_subnets_cidr_blocks
+  private_route_table_ids     = module.vpc.private_route_table_ids
+}
+
+resource "aws_eip" "nat" {
+  network_interface = module.nat.eni_id
+  tags = {
+    "Name" = "nat-instance-main"
+  }
+}
+
 resource "aws_iam_policy" "worker_policy" {
   name        = "worker-policy"
   description = "Worker policy for the ALB Ingress"
