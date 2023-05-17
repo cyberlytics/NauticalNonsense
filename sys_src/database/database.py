6e3f8701e0f3de15450745d308b6bfb8be85245d
Fabian Wagner <f.wagner@oth-aw.de>
2023-05-17 15:13:16 +0200

    added examples for pymongo
    Former-commit-id: c7093548f6d56b248a9c757af5338e4843ffabc8

--- sys_src/database/database.py
+++ sys_src/database/database.py
@@ -8,3 +8,30 @@ games = db.games
 
 #Example: Get last entry in collection
 game = games.find_one()
+
+#Example: Insert one entry in collection
+doc = {"game_id": 3,
+       "current_player": 1,
+       "board":{"11": 1, "12": 1, "21": 0, "22": 2}}
+doc_id = str(games.insert_one(doc).inserted_id)
+
+#Examples: Insert more entries in collection
+doc2 = {"game_id": 1,
+       "current_player": 2,
+       "board":{"11": 0, "12": 1, "21": 1, "22": 2}}
+doc3 = {"game_id": 2,
+       "current_player": 1,
+       "board":{"11": 0, "12": 0, "21": 0, "22": 0}}
+doc4 = {"game_id": 2,
+       "current_player": 2,
+       "board":{"11": 1, "12": 0, "21": 0, "22": 0}}
+inserted_games = games.insert_many([doc2,doc3,doc4])
+
+#Example: Get all entries in collection
+all_games = games.find()
+
+#Example: Get all entries for specific game_id
+game_with_id1 = games.find({"game_id": 1})
+#Iterate over cursor
+for game1 in game_with_id1:
+    print(game1)
