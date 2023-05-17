cfe18755e7fffdf0ce8b52f04626d7dd915f7f56
Jakob Götz <j.goetz2@oth-aw.de>
2023-05-17 23:01:55 +0200

    added websocket route and example
    Former-commit-id: 4fe18f49789b7405f12a1368cee9400bde3b3ee4

--- sys_src/backend/main.py
+++ sys_src/backend/main.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, WebSocket
+import random
 
 app = FastAPI()
 
@@ -9,8 +10,18 @@ def startpage():
     return {"message": "Hello World"}
 
 
-@app.get("/polling/")
+@app.get("/polling")
 def polling():
     global polling_count
     polling_count += 1
     return {"message": f"Es wurde {polling_count} Mal gepollt"}
+
+
+#todo: multiple persistente websocket verbindungen
+@app.websocket("/ws")
+async def websocket(websocket: WebSocket):
+    await websocket.accept()
+    #data = await websocket.receive_json()
+    # do something with data
+    await websocket.send_json({"msg": f"Hello WebSocket"})
+    await websocket.close()
