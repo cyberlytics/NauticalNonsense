cd69d364e63290f2df516e5745c661a300ad98ec
Jakob Götz <j.goetz2@oth-aw.de>
2023-05-18 20:32:22 +0200

    websockets working with multiple clients and docker
    Former-commit-id: 84a67e821255dccaef5f903b4de0a9388fac518a

--- sys_src/backend/main.py
+++ sys_src/backend/main.py
@@ -1,9 +1,10 @@
 from fastapi import FastAPI, WebSocket
-import random
+
 
 app = FastAPI()
 
 polling_count = 0
+
 
 @app.get("/")
 def startpage():
@@ -17,11 +18,9 @@ def polling():
     return {"message": f"Es wurde {polling_count} Mal gepollt"}
 
 
-#todo: multiple persistente websocket verbindungen
 @app.websocket("/ws")
-async def websocket(websocket: WebSocket):
+async def websocket_endpoint(websocket: WebSocket):
     await websocket.accept()
-    #data = await websocket.receive_json()
-    # do something with data
-    await websocket.send_json({"msg": f"Hello WebSocket"})
-    await websocket.close()
+    while True:
+        data = await websocket.receive_text()
+        await websocket.send_text(f"Message text was: {data}")
