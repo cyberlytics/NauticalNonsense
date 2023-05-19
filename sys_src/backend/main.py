46b3230fe8445e3d4eb186e89e7fdded4a24604a
Jakob Götz <j.goetz2@oth-aw.de>
2023-05-19 14:55:10 +0200

    added corsmiddleware, route for game start and websocket broadcast
    Former-commit-id: bf3530a00f8893ba1b5468fd6933e82732dbbf94

--- sys_src/backend/main.py
+++ sys_src/backend/main.py
@@ -1,8 +1,21 @@
-from fastapi import FastAPI, WebSocket
-
+from fastapi import FastAPI, WebSocket, WebSocketDisconnect
+from play_game import existing_game, validate_move, get_new_room
+from websocket_manager import ConnectionManager
+import uuid
+from fastapi.responses import JSONResponse
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:8080"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+manager = ConnectionManager()
 polling_count = 0
 
 
@@ -18,9 +31,48 @@ def polling():
     return {"message": f"Es wurde {polling_count} Mal gepollt"}
 
 
-@app.websocket("/ws")
-async def websocket_endpoint(websocket: WebSocket):
-    await websocket.accept()
-    while True:
-        data = await websocket.receive_text()
-        await websocket.send_text(f"Message text was: {data}")
+@app.get("/against_random")
+def against_random():
+    client_id = uuid.uuid4().int
+    room_id = get_new_room(client_id, "random")
+    return JSONResponse({"websocket_route": f"ws/{room_id}"})
+
+
+@app.post("/against_friend")
+def against_friend(name: str):
+    client_id = uuid.uuid4().int
+    room_id = get_new_room(client_id, "friend")
+    return JSONResponse({"websocket_route": f"ws/{room_id}"})
+    
+
+@app.get("/against_computer")
+def against_computer():
+    client_id = uuid.uuid4().int
+    room_id = get_new_room(client_id, "computer")
+    return JSONResponse({"websocket_route": f"ws/{room_id}"})
+
+
+# Diese Route wird vom Frontend aufgerufen, wenn die Verbindung bei den Websockets abbricht (ohne sieger)
+# reconnect to most recent game played with the player_id
+@app.get("/continue")
+def continue_game(player_id: int):
+    pass
+
+
+# room_id wird die session id
+@app.websocket("/ws/{session_id}")
+async def websocket_endpoint(websocket: WebSocket, session_id: int):
+    await manager.connect(websocket)
+    try:
+        while True:
+            # check if two guys are in one room
+            # if ready(): 
+            data = await websocket.receive_json()
+            # validate the data
+            response = {"message received in the backend": data}
+            await manager.send_personal_message(str(response), websocket)
+            await manager.broadcast(f"Client #{session_id} sent: {str(data)}")
+    except WebSocketDisconnect:
+        manager.disconnect(websocket)
+        await manager.broadcast(f"Client #{session_id} left")
+
---
+++ sys_src/backend/play_game.py
@@ -0,0 +1,28 @@
+import uuid
+
+def get_new_room(client_id: int, game_type: str, name: str):
+    '''
+    Check in Database if a map exists, which the client can use.
+
+    If not, construct a new map.
+        The map is first empty (new_game_init has to be called).
+    if there is a existing map with a player waiting, use it (friend or other random guy is waiting)
+    return the room_id
+    '''
+    return uuid.uuid4().int
+
+def new_game_init():
+    # place ships
+    pass
+
+def existing_game(client_json):
+    # search in game_id in client_json. if it's there, then reuse this game
+    # if this is not already existing in the database start a new game
+    pass
+
+def validate_move(client_json):
+    # check if the game_field key in client_json only changed in one position.
+    # check if all fields are the same (game_id, and more), only step and one position in game_field should be off.
+    # step size has to be one more then the previous, nothing else
+    # only one move per player, keep care that one player dont send more times a move
+    pass
