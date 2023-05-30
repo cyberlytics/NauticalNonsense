2c12f2946b5b139a3e60fe9ff3128155cf0227a9
Fabian Wagner <f.wagner@oth-aw.de>
2023-05-30 15:53:20 +0200

    added get_stat und update_stats
    Former-commit-id: cbd76a58906d9578714532943cfdd5fef25c0c10

--- sys_src/database/database.py
+++ sys_src/database/database.py
@@ -2,7 +2,7 @@ import pymongo
 from pymongo import MongoClient
 from pymongo.server_api import ServerApi
 import math
-from models import State, Winner, WinnerWithRank
+from models import State, Winner, WinnerWithRank, Stat
 
 mongo_url = "mongodb+srv://nn_user:nn_bsyjntss@nauticalnonsens.lflmzfg.mongodb.net/?retryWrites=true&w=majority"
 client = MongoClient(mongo_url, server_api=ServerApi('1'))
@@ -11,7 +11,7 @@ gamestates = db.gamestates
 stats = db.stats
 leaderboard = db.leaderboard
 
-
+#Gamestates
 def get_current_state(game_id: str) -> State:
     current_state = gamestates.find_one({"game_id": game_id}, sort=[("timestamp",pymongo.DESCENDING)])
     if not current_state:
@@ -23,6 +23,7 @@ def save_state(state: State) -> State:
     return state
 
 
+#Leaderboard
 def get_leaderboard(againstComputer:bool, limit: int = 10) -> list[Winner]:
     leaders = []
     winners = leaderboard.find({"againstComputer": againstComputer}, sort=[("moves",pymongo.ASCENDING), ("name",pymongo.ASCENDING)], limit=limit)
@@ -33,8 +34,10 @@ def get_leaderboard(againstComputer:bool, limit: int = 10) -> list[Winner]:
 def insert_winner(current_state: State) -> Winner:
     name = current_state.winner
     moves = math.ceil(current_state.step/2)
-    againstComputer = current_state.againstComputer
-    
+    if current_state.gameMode == "pc":
+        againstComputer = True
+    else:
+        againstComputer = False    
     winner = Winner(name=name, moves=moves, againstComputer=againstComputer)
     leaderboard.insert_one(winner.dict())
     return winner
@@ -49,3 +52,68 @@ def add_rank(leaders = list[Winner]) -> list[WinnerWithRank]:
             current_moves = leader.moves
         leaders_rank.append(WinnerWithRank(name=leader.name,moves=leader.moves,rank=rank))
     return leaders_rank
+
+
+#Gamestatistics
+def get_stat() -> Stat:
+    stat = stats.find_one({}, sort=[("timestamp",pymongo.DESCENDING)])
+    return Stat.parse_obj(stat)
+
+def update_stats(end_state: State) -> Stat:
+    #old stats
+    stat_old = get_stat()
+    
+    #make new stats
+    stat = stat_old.copy()
+    
+    stat.gamesCount += 1
+    stat.totalMoves += end_state.step    
+    if end_state.gameMode == "pc":
+        stat.gamesCountComputer += 1
+        stat.totalMovesComputer += end_state.step
+        if end_state.winner == "pc":
+            winCountComputer += 1            
+    else:
+        stat.gamesCountHuman += 1
+        stat.totalMovesHuman += end_state.step
+    stat.averageMoves = stat.totalMoves / stat.gamesCount
+    if stat.gamesCountHuman != 0:
+        stat.averageMovesHuman = stat.totalMovesHuman / stat.gamesCountHuman
+    if stat.gamesCountComputer != 0:
+        stat.averageMovesComputer = stat.totalMovesComputer / stat.gamesCountComputer
+    
+    shipPositions1 = [1 if pos==1 or pos==3 or pos==4 else 0 for pos in end_state.board1] #part of ship if 1,3,4
+    shipPositions2 = [1 if pos==1 or pos==3 or pos==4 else 0 for pos in end_state.board2]
+    stat.shipPositions = [sum(p) for p in zip(stat.shipPositions, shipPositions1)]
+    stat.shipPositions = [sum(p) for p in zip(stat.shipPositions, shipPositions2)]
+    
+    moves1 = [1 if pos>=2 else 0 for pos in end_state.board1] #field clicked if >=2
+    moves2 = [1 if pos>=2 else 0 for pos in end_state.board2]
+    stat.moves = [sum(p) for p in zip(stat.moves, moves1)]
+    stat.moves = [sum(p) for p in zip(stat.moves, moves2)]
+    
+    #for move in end_state.firstMoves: #in end_state.firstMoves die zwei ersten geklickten Felder
+    #    stat.firstMoves[move] += 1
+        
+    #popularShipPositions
+    
+    if end_state.winner == end_state.player1:
+        ships_winner = end_state.ships1
+    else:
+        ships_winner = end_state.ships2
+        
+    for ship in ships_winner:
+        if any(pos > 100 for pos in ship):
+            stat.totalShipsHit[len(ship)-1] += 1
+    stat.averageShipsHit = [total / (factor*stat.gamesCount) for total, factor in zip(stat.totalShipsHit, [2,2,1,1,1])] #number of ships per type #oder gamesCountHuman
+    
+    for ship in ships_winner:
+        for pos in ship:
+            if pos > 100:
+                stat.totalShiphits[len(ship)-1] += 1                
+    stat.averageShiphits = [total / (factor*stat.gamesCount) for total, factor in zip(stat.totalShiphits, [2,4,3,4,5])] #number of tiles per shiptype #oder gamesCountHuman
+    
+    stat.timestamp = datetime.datetime.utcnow()    
+    
+    stats.insert_one(stat.dict())
+    return stat
--- sys_src/database/models.py
+++ sys_src/database/models.py
@@ -6,7 +6,7 @@ class State(BaseModel):
     player1: str = Field(..., description = "Name of player 1")
     player2: str = Field(..., description = "Name of player 2")
     next_player: str = Field(..., description = "Name of next player in turn")
-    againstComputer: bool = Field(..., description = "True if opponent is computer")
+    gameMode: str = Field(..., description = "against pc/random/friend")
     isFinished: bool = Field(..., description = "True if game is over")
     winner: str = Field(..., description = "Name of player, who won")
     step: int = Field(..., description = "Count of total moves of game")
@@ -45,10 +45,10 @@ class Stat(BaseModel):
     averageMovesComputer: float = Field(..., description = "Average number of moves until end of game against computer") #vllt /2 ?
     shipPositions: list[int] = Field(..., description = "Total positions of ships")
     moves: list[int] = Field(..., description = "(Normalized) total moves") #Mongo: Absolut, Frontend: Relativ ?
-    firstMoves: list[int] = Field(..., description = "Most commonly chosen first moves")
+    #firstMoves: list[int] = Field(..., description = "Most commonly chosen first moves")
     #popularShipPositions: list[list[int]] = Field(..., description = "Most popular ship positions")
-    totalShipsHit: list[int] = Field(..., description = "Total count of loser's ships hit per shiptype by end of game")#Zahl der mind 1x getroffenen Schiffe #vllt auch Gesamtanzahl
-    averageShipsHit: list[float] = Field(..., description = "Average count of loser's ships hit per shiptype by end of game")
-    totalShiphits: list[int] = Field(..., description = "Total count of loser's ships' hits per shiptype by end of game")#Anteile der Schiffe, die getroffen sind
-    averageShiphits: list[float] = Field(..., description = "Average count of loser's ships' hits per shiptype by end of game")
+    totalShipsHit: list[int] = Field(..., description = "Total count of winner's ships hit per shiptype by end of game")#Zahl der mind 1x getroffenen Schiffe #vllt auch Gesamtanzahl
+    averageShipsHit: list[float] = Field(..., description = "Average count of winner's ships hit per shiptype by end of game")
+    totalShiphits: list[int] = Field(..., description = "Total count of winner's ships' hits per shiptype by end of game")#Anteile der Schiffe, die getroffen sind
+    averageShiphits: list[float] = Field(..., description = "Average count of winner's ships' hits per shiptype by end of game")
     timestamp: datetime.datetime = Field(..., description = "Current utc date and time")
