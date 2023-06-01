73efb19667ccc60d93d69573584561a7cc0f538a
Fabian Wagner <f.wagner@oth-aw.de>
2023-06-01 11:45:22 +0200

    added return button
    Former-commit-id: 4c55c240497fab67ccce64e3420db177412c9982

--- sys_src/frontend/NauticalNonsense/src/scenes/Statistics.js
+++ sys_src/frontend/NauticalNonsense/src/scenes/Statistics.js
@@ -16,11 +16,34 @@ class Statistics extends Phaser.Scene {
 	/** @returns {void} */
 	editorCreate() {
 
+		//reference to this statistics scene
+		const statistics = this;
+
 		// background
 		const background = this.add.image(0, 0, "0001");
 		background.scaleX = 1.2;
 		background.scaleY = 0.7;
 		background.setOrigin(0, 0);
+
+		//returnButton
+		const returnButton = this.add.image(70, 650, "optionsBack").setInteractive({ useHandCursor: true })
+		returnButton.scaleX = 0.7;
+		returnButton.scaleY = 0.7;
+
+		returnButton.on('pointerover', function (event) {
+			this.setTint(0x808080);
+		})
+
+		returnButton.on('pointerout', function (event) {
+			this.clearTint();
+		})
+
+		returnButton.on('pointerdown', function (event) {
+			statistics.sound.add("click").play();
+			this.clearTint();
+			statistics.scene.start('Options');
+		})
+
 
 		// get stats via backend API
 		//...
