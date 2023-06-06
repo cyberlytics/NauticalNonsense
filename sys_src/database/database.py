447184c6e9cfb0c2425439583ed51f7de809fd55
Fabian Wagner <f.wagner@oth-aw.de>
2023-06-06 13:40:23 +0200

    updated models for state and statistics
    Former-commit-id: 633c3f063824066c8c0de2c71e7d6babe322ae95

--- sys_src/database/database.py
+++ sys_src/database/database.py
@@ -11,6 +11,9 @@ gamestates = db.gamestates
 stats = db.stats
 leaderboard = db.leaderboard
 
+shipsCount = [2,2,1,1,1] #number of ships per type
+shipsTiles = [2,4,3,4,5] #number of tiles per shiptype
+
 #Gamestates
 def get_current_state(game_id: str) -> State:
     current_state = gamestates.find_one({"game_id": game_id}, sort=[("timestamp",pymongo.DESCENDING)])
@@ -33,7 +36,7 @@ def get_leaderboard(againstComputer:bool, limit: int = 10) -> list[Winner]:
 
 def insert_winner(current_state: State) -> Winner:
     name = current_state.winner
-    moves = math.ceil(current_state.step/2)
+    moves = math.ceil(current_state.step/2)#nochmal dran nach Treffer?
     if current_state.gameMode == "pc":
         againstComputer = True
     else:
@@ -59,7 +62,7 @@ def get_stat() -> Stat:
     stat = stats.find_one({}, sort=[("timestamp",pymongo.DESCENDING)])
     return Stat.parse_obj(stat)
 
-def update_stats(end_state: State) -> Stat:
+def update_stats(end_state: State, capitulation: bool) -> Stat:
     #old stats
     stat_old = get_stat()
     
@@ -76,12 +79,16 @@ def update_stats(end_state: State) -> Stat:
     else:
         stat.gamesCountHuman += 1
         stat.totalMovesHuman += end_state.step
-    stat.averageMoves = stat.totalMoves / stat.gamesCount
+    if stat.gamesCount !=0:
+        stat.averageMoves = stat.totalMoves / stat.gamesCount
     if stat.gamesCountHuman != 0:
         stat.averageMovesHuman = stat.totalMovesHuman / stat.gamesCountHuman
     if stat.gamesCountComputer != 0:
         stat.averageMovesComputer = stat.totalMovesComputer / stat.gamesCountComputer
     
+    if capitulation == True:
+        stat.capitulations += 1
+
     shipPositions1 = [1 if pos==1 or pos==3 or pos==4 else 0 for pos in end_state.board1] #part of ship if 1,3,4
     shipPositions2 = [1 if pos==1 or pos==3 or pos==4 else 0 for pos in end_state.board2]
     stat.shipPositions = [sum(p) for p in zip(stat.shipPositions, shipPositions1)]
@@ -95,9 +102,8 @@ def update_stats(end_state: State) -> Stat:
     #for move in end_state.firstMoves: #in end_state.firstMoves die zwei ersten geklickten Felder
     #    stat.firstMoves[move] += 1
         
-    #popularShipPositions
     
-    if end_state.winner == end_state.player1:
+    if end_state.winner == end_state.player1Name:
         ships_winner = end_state.ships1
     else:
         ships_winner = end_state.ships2
@@ -105,13 +111,13 @@ def update_stats(end_state: State) -> Stat:
     for ship in ships_winner:
         if any(pos > 100 for pos in ship):
             stat.totalShipsHit[len(ship)-1] += 1
-    stat.averageShipsHit = [total / (factor*stat.gamesCount) for total, factor in zip(stat.totalShipsHit, [2,2,1,1,1])] #number of ships per type #oder gamesCountHuman
+    stat.averageShipsHit = [total / (factor*stat.gamesCount) for total, factor in zip(stat.totalShipsHit, shipsCount)]#oder gamesCountHuman
     
     for ship in ships_winner:
         for pos in ship:
             if pos > 100:
                 stat.totalShiphits[len(ship)-1] += 1                
-    stat.averageShiphits = [total / (factor*stat.gamesCount) for total, factor in zip(stat.totalShiphits, [2,4,3,4,5])] #number of tiles per shiptype #oder gamesCountHuman
+    stat.averageShiphits = [total / (factor*stat.gamesCount) for total, factor in zip(stat.totalShiphits, shipsTiles)]#oder gamesCountHuman
     
     stat.timestamp = datetime.datetime.utcnow()    
     
